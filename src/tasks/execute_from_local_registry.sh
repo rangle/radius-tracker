ca64b380969bea1cdbea16373389db446eedd8ec
Arseny Smoogly <arseny.smoogly@rangle.io>
2022-03-15 13:17:48 +0100

    Print version when test-executing from local registry

--- src/tasks/execute_from_local_registry.sh
+++ src/tasks/execute_from_local_registry.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-# TODO: Ideally, test the tracker output is sane, instead of using `--help`
-NPM_CONFIG_REGISTRY=http://localhost:8080 npx -p radius-tracker@latest radius-tracker . --help
+# TODO: Ideally, test the tracker output is sane, instead of using `--version`
+NPM_CONFIG_REGISTRY=http://localhost:8080 npx -p radius-tracker@latest radius-tracker . --version
