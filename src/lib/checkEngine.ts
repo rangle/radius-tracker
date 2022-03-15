df54943bdfd404baa1806b3191f57990845d2c36
Pavel Ivanov <pablospaniard@users.noreply.github.com>
2022-03-15 11:14:55 +0100

    check if npm is used

--- src/lib/checkEngine.ts
+++ src/lib/checkEngine.ts
@@ -1,5 +1,7 @@
 import { satisfies } from "semver";
 
+import * as commandExists from "command-exists";
+
 const getPkg = () => {
     try { return require("../package.json"); } // In build
     catch (_ignore) { return require("../../package.json"); } // In dev
@@ -8,4 +10,8 @@ const requiredVersion = getPkg().engines.node;
 
 if (!satisfies(process.version, requiredVersion)) {
     throw new Error(`Unsupported Node version. Expected ${ requiredVersion }, got ${ process.version }`);
+}
+
+if (commandExists.sync("npm")) {
+    throw new Error("Please use yarn instead of npm");
 }
