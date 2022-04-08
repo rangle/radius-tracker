83bff5b6f68b777f73aa28a1e395344a9cb1ab61
Pavel Ivanov <pablospaniard@users.noreply.github.com>
2022-04-08 13:12:16 +0200

    node m odules removed from git

--- src/lambda/listener/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-exports.handler = (event) => tslib_1.__awaiter(void 0, void 0, void 0, function* () {
-    console.log("EVENT\n" + JSON.stringify(event));
-    return { statusCode: 200, body: "success" };
-});
