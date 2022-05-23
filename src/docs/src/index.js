622308af16350dd66266a145f51daae4935c6d09
Pavel Ivanov <pablospaniard@users.noreply.github.com>
2022-05-23 17:17:33 +0200

    docs first build

---
+++ src/docs/src/index.js
@@ -0,0 +1,11 @@
+import Markdoc from "@markdoc/markdoc";
+
+const source = "# Markdoc";
+
+const ast = Markdoc.parse(source);
+const content = Markdoc.transform(ast);
+
+const html = Markdoc.renderers.html(content);
+
+// eslint-disable-next-line no-undef
+document.getElementById("markdoc").innerHTML = html;
