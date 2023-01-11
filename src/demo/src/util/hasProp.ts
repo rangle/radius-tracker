045a69e06737003c063ee2442f7d32de16906855
Pavel Ivanov <pablospaniard@gmail.com>
2023-01-11 16:19:36 +0100

    fixed

--- src/demo/src/util/hasProp.ts
+++ src/demo/src/util/hasProp.ts
@@ -1,2 +1,5 @@
 // TODO: duplicate of `hasProp` in lib/guards — remove duplicate
-export const hasProp = <P extends string>(prop: P) => <T>(val: T): val is T & { [p in P]: unknown } => Boolean(val) && typeof val === "object" && prop in val;
+export const hasProp = <P extends string>(prop: P) => <T>(val: T): val is T & { [p in P]: unknown } => {
+    if (!val) { return false; }
+    return Boolean(val) && typeof val === "object" && prop in val;
+};
