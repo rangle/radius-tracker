51301e77c592553e75c750eb67070b1a3059b44a
riavalon <raven.avalon5@gmail.com>
2023-01-26 17:12:26 -0330

    Nextra Documentation init and GH actions updates

---
+++ src/docs/theme.config.jsx
@@ -0,0 +1,12 @@
+export default {
+    logo: <span>Radius Tracker</span>,
+    project: {
+        link: "https://github.com/rangle/radius-tracker",
+    },
+
+    docsRepositoryBase: "https://github.com/rangle/radius-tracker",
+
+    toc: {
+        extraContent: null,
+    },
+}
