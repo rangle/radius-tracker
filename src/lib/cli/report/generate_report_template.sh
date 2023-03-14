a02c5e0594d068da94555173d91ba6934d4027da
Arseny Smoogly <arseny.smoogly@rangle.io>
2023-03-14 13:21:06 +0100

    Local report generator
    Fix template generation in package —
    no ts-node available or needed.

--- src/lib/cli/report/generate_report_template.sh
+++ src/lib/cli/report/generate_report_template.sh
@@ -23,7 +23,8 @@ rm report.tgz
 
 # Run transformations
 BASEDIR=$(dirname "$0")
-npx --package ts-node -- node -r ts-node/register/transpile-only "${BASEDIR}/prepare_report" "${REPORT_DIR}"
+[ -e "${BASEDIR}/prepare_report.js" ] && node "${BASEDIR}/prepare_report.js" "${REPORT_DIR}" # In package
+[ -e "${BASEDIR}/prepare_report.ts" ] && node -r ts-node/register/transpile-only "${BASEDIR}/prepare_report.ts" "${REPORT_DIR}" # In dev
 
 # Append additional styles
 cat "${BASEDIR}/additional_styles.css" >> "${REPORT_DIR}/inspector.css"
