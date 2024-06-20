2ca00360f34d529ed772a63ff5696c2f1f4ed35d
Arseny Smoogly <arseny@smoogly.dev>
2024-06-20 12:57:44 +0200

    Rename TodoMVC react subproject, as it has been moved in their repo

--- src/lib/cli/test/react_todoapp.js
+++ src/lib/cli/test/react_todoapp.js
@@ -1,7 +1,7 @@
 // React TodoApp from https://github.com/tastejs/todomvc/blob/4e301c7014093505dcf6678c8f97a5e8dee2d250/examples/react-hooks
 module.exports = [{
     repoUrl: "https://github.com/tastejs/todomvc",
-    subprojectPath: "examples/react-hooks",
+    subprojectPath: "examples/react",
     isTargetModuleOrPath: /react-router-dom/, // Pretend react-router is a lib we want to track
     maxWeeks: 1,
 }];
